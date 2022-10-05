bd89fb2c8a0e530ea7173bdd1318df747b6ec91d
Amun1804 <gregoryjoseph@live.com>
2022-10-05 00:41:12 -0400

    updated WTForms

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from flask_wtf.file import DataRequired
-from wtforms import TextField, PasswordField
-
+from wtforms import TextField, PasswordField, SubmitField, validators, ValidationError
 
 app = Flask(__name__)
 app.secret_key = "this is a secret key"
 
 
+def valid_email(form, field):
+    if '@' not in field and '.' not in str(field):
+        raise ValidationError('Invalid email address!')
+
+
 class LoginForm(FlaskForm):
-    username = TextField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = TextField(label='Username', validators=[DataRequired(),
+                                                       validators.Length(min=8, message='username is too short'),
+                                                       validators.Email(message='not valid email format')])
+    password = PasswordField(label='Password', validators=[DataRequired(),
+                                                           validators.Length(min=8,
+                                                                             message='Password must be at least  8 characters long.')])
+    submit = SubmitField(label='log In')
 
 
 @app.route('/')
@@ -18,9 +27,14 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'admin@email.com' and form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=form)
 
 
